4dc27ca68d66d5875a75caeb600a40bd9ea42e97
yuky1016 <yaona1016@163.com>
2018-05-14 16:49:54 +0800

    Initial commit

---
+++ common/Public/pub.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+# edit by yuki
+# python version：python2
+
+import sys
+import os
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0,BASEPATH)
+import logging
